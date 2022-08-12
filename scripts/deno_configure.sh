69caa1244e9d4e0a6326be202a32f14734286837
Siddhant Khare <siddhant@gitpod.io>
2022-08-12 19:51:10 +0000

    mend

--- scripts/deno_configure.sh
+++ scripts/deno_configure.sh
@@ -5,5 +5,5 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 
 target="$HOME/.deno";
 
-echo 'export %s\n' "DENO_INSTALL=$target"\
+echo 'export \n' "DENO_INSTALL=$target"\
                                 "PATH=$target/bin:\$PATH" >> $HOME/.bashrc.d/600-deno
